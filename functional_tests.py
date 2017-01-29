967e062094d74e765a8e8616a7136431cbf9a66c
mish24 <poonammishra24398@gmail.com>
2017-01-29 21:46:43 +0530

    updated the functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,43 +1,46 @@
+#to check from the point of view of a user
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest (unittest.TestCase):
+class NewVisitorTest(unittest.TestCase):
 	
 	def setUp(self):
 		self.browser = webdriver.Firefox()
-		self.implicitly_wait(3)
 		
 	def tearDown(self):
 		self.browser.quit()
 		
-	def test_can_start_a_list_and_retrieve_it_later(self):
-		#you learnt about a new cool website and you went online to check it out
+	def test_can_start_a_new_list_and_retrieve_it_later(self):
+		#let's check out the homepage
 		self.browser.get('http://localhost:8000')
 		
-		#you should notice the page title and header mention to-do lists
+	#you notice that the title and header are mentioned
 		self.assertIn('To-Do', self.browser.title)
+		
+		#next, we check whether the header we talked about is pro=esent or not. ALso, doe it have right info
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
 		
-		#you are invited to enter a to-do item straightaway
+		#you are really invited to enter a new item to the list
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
-		
-		#she types "Buy peacock feathers" into the text box
-		inputbox.send_keys('Buy peacock feathers')
-		
-		#when she hits enter, the page updates and now the page lists 1: Buy peacock feathers
+		#you type get gsoc into the textbox
+		inputbox.send_keys('Get GSoC')
+		#when you hit tinter, only then the page updates
 		inputbox.send_keys(Keys.ENTER)
 		
+		#next we move to the next element, which tells us about the table
+		#remember, sometimes we need to just find the element, other times we need to access whats inside that specific id
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.asserttrue(any(row.text == '1: Buy peacock feathers' for row in rows)
+		#we add the second argument as the error message
+		self.assertTrue(any(row.text == '1. Get GSoC' for row in rows), "New item did  not appear in the table")
 		
-		#there still is a box to invite her to enter another item, she enters "Use peacock feathers to make a fly"
-
+		#there is still a text box inviting you to add another item. 
+		#for later
+		
 		
 		
 if __name__ == '__main__':
-	unittest.main(warnings='ignore')
-	#for the reminder, the name and main thing ensures that the program is run from the command line and not from any other script
+	unittest.main()
